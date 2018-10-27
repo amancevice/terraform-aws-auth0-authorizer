00e06c9faa7ffed84e7e0412544177643534e4bc
Alexander Mancevice <amancevice@cargometrics.com>
2018-10-27 15:34:02 -0400

    improved examples

--- example/main.tf
+++ example/main.tf
@@ -1,9 +1,9 @@
 provider aws {
+  version    = "~> 1.41"
   access_key = "${var.aws_access_key_id}"
   secret_key = "${var.aws_secret_access_key}"
   region     = "${var.aws_region}"
   profile    = "${var.aws_profile}"
-  version    = "~> 1.41"
 }
 
 module authorizer {
@@ -11,4 +11,35 @@ module authorizer {
   authorizer_audience     = "${var.authorizer_audience}"
   authorizer_jwks_uri     = "${var.authorizer_jwks_uri}"
   authorizer_token_issuer = "${var.authorizer_token_issuer}"
+}
+
+resource aws_api_gateway_rest_api api {
+  description = "Example API"
+  name        = "auth0"
+}
+
+resource aws_api_gateway_method any {
+  authorization = "CUSTOM"
+  authorizer_id = "${aws_api_gateway_authorizer.authorizer.id}"
+  http_method   = "ANY"
+  resource_id   = "${aws_api_gateway_rest_api.api.root_resource_id}"
+  rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
+}
+
+resource aws_api_gateway_integration any {
+  http_method = "${aws_api_gateway_method.any.http_method}"
+  resource_id = "${aws_api_gateway_rest_api.api.root_resource_id}"
+  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
+  type        = "MOCK"
+
+  request_templates {
+    "application/json" = "{\"statusCode\": 200}"
+  }
+}
+
+resource aws_api_gateway_authorizer authorizer {
+  authorizer_credentials = "${module.authorizer.lambda_role_arn}"
+  authorizer_uri         = "arn:aws:apigateway:${var.aws_region}:lambda:path/2015-03-31/functions/${module.authorizer.lambda_arn}/invocations"
+  name                   = "auth0"
+  rest_api_id            = "${aws_api_gateway_rest_api.api.id}"
 }
--- example/outputs.tf
+++ example/outputs.tf
@@ -1,24 +1,24 @@
-output lambda_arn {
+output authorizer_lambda_arn {
   description = "API Lambda ARN."
   value       = "${module.authorizer.lambda_arn}"
 }
 
-output lambda_name {
+output authorizer_lambda_name {
   description = "API Lambda name."
   value       = "${module.authorizer.lambda_name}"
 }
 
-output lambda_role_arn {
+output authorizer_lambda_role_arn {
   description = "Lambda role ARN."
   value       = "${module.authorizer.lambda_role_arn}"
 }
 
-output lambda_role_name {
+output authorizer_lambda_role_name {
   description = "Lambda role name."
   value       = "${module.authorizer.lambda_role_name}"
 }
 
-output lambda_role_policy_arns {
+output authorizer_lambda_role_policy_arns {
   description = "Lambda role policy ARNs."
   value       = ["${module.authorizer.lambda_role_policy_arns}"]
 }
